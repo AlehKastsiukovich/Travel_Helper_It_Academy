4a3d885d80f9a5a0f2255ead93be6c3d5f968bd3
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-11-14 14:37:55 +0300

    add view binding to CountryListFragment

--- app/src/main/java/by/itacademy/training/travelhelper/view/CountryListFragment.kt
+++ app/src/main/java/by/itacademy/training/travelhelper/view/CountryListFragment.kt
@@ -1,12 +1,22 @@
 package by.itacademy.training.travelhelper.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import by.itacademy.training.travelhelper.databinding.FragmentCountryListBinding
 
 class CountryListFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private lateinit var binding: FragmentCountryListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCountryListBinding.inflate(inflater, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
