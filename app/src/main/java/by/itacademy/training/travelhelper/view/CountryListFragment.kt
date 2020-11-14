95b7c43b11dde1f5db63b19c460245a22694b8a0
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-11-14 14:22:28 +0300

    start project

---
+++ app/src/main/java/by/itacademy/training/travelhelper/view/CountryListFragment.kt
@@ -0,0 +1,12 @@
+package by.itacademy.training.travelhelper.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class CountryListFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/by/itacademy/training/travelhelper/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.itacademy.training.travelhelper.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.itacademy.training.travelhelper.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
