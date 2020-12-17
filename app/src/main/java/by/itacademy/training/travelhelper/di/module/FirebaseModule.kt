920d46b960976820cc660ca2e18521050a2b10ec
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-12-17 16:26:35 +0300

    add Event class

--- app/src/main/java/by/itacademy/training/travelhelper/di/module/FirebaseModule.kt
+++ app/src/main/java/by/itacademy/training/travelhelper/di/module/FirebaseModule.kt
@@ -5,10 +5,12 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class FirebaseModule {
 
+    @Singleton
     @Provides
     fun provideFirebaseFireStore(): FirebaseFirestore = Firebase.firestore
 }
