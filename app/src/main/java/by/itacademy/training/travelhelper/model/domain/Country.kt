8e697e8d8891dbb6d65a40123f69c6c0d2e95016
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-12-20 19:26:13 +0300

    add domain Country class

---
+++ app/src/main/java/by/itacademy/training/travelhelper/model/domain/Country.kt
@@ -0,0 +1,11 @@
+package by.itacademy.training.travelhelper.model.domain
+
+data class Country(
+    val name: String,
+    val region: String,
+    val description: String,
+    val imageUrl: String,
+    val descriptionImageUrl: String,
+    val capital: String,
+    val language: String
+)
