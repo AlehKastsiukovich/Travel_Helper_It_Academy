969374939d0b8520a4bfe97ddd7c40f8b5bb7733
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2021-01-02 13:29:44 +0300

    add OnRouteClickListener interface

---
+++ app/src/main/java/by/itacademy/training/travelhelper/ui/adapter/OnRouteClickListener.kt
@@ -0,0 +1,8 @@
+package by.itacademy.training.travelhelper.ui.adapter
+
+import by.itacademy.training.travelhelper.model.domain.Route
+
+interface OnRouteClickListener {
+
+    fun onRouteClick(route: Route)
+}
