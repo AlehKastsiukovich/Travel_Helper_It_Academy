693d917b99f4cad97e49072f3d10945b1f5d5841
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2021-01-24 17:31:38 +0300

    remove empty routs RouteTypeFragment

--- app/src/main/java/by/itacademy/training/travelhelper/ui/view/RouteTypeFragment.kt
+++ app/src/main/java/by/itacademy/training/travelhelper/ui/view/RouteTypeFragment.kt
@@ -47,8 +47,6 @@ class RouteTypeFragment : Fragment() {
                     startRouteListFragment()
                 }
             }
-
-            hikingView.setOnClickListener { }
         }
     }
 
